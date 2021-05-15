33400eb7c11dd4fb50fb3312114415c71312579b
Vienio99 <kyve96@gmail.com>
2021-05-15 22:02:18 +0200

    adding branches

--- src/index.js
+++ src/index.js
@@ -5,42 +5,98 @@ import Branch from './modules/branchGenerator';
 
     const tasks = document.querySelector('.task-list');
     const branch = document.getElementById('current-branch');
-    let form = document.getElementById('task-form');
+    const lastTask = tasks.lastChild;
 
+    let taskForm = document.getElementById('task-form');
+    let branchForm = document.getElementById('branch-form')
 
-    form.addEventListener('submit', (e) => {
+    let taskList = [];
+
+    // Form submit listener
+    taskForm.addEventListener('submit', (e) => {
         e.preventDefault();
+        addNewTask();
+        clearTaskForm();
+    });
 
-        const lastTask = tasks.lastChild;
-        console.log(lastTask)
-        // Create task and append it to the list
+    branchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        addNewBranch();
+        clearBranchForm();
+    });
+
+    // Add new task to the list
+    let addNewTask = function() {
+
         const newTask = document.createElement('li');
         newTask.setAttribute('class', 'task')
 
+        // Check if there is last task to number it properly
         if (lastTask) {
             newTask.setAttribute('id', Number(lastTask.id) + 1)
         } else {
             newTask.setAttribute('id', '1')
         }
 
+        // Create new task object
 
-        const todo = new Todo(form.newTaskTitle.value, '12.03.2012', 'Very important');
+        const todo = new Todo(taskForm.newTaskTitle.value, '12.03.2012', 'Very important');
+        taskList.push(todo);
+
+        // Add necessary elements
+
         tasks.appendChild(newTask);
-        const icon = document.createElement('span');
-        icon.setAttribute('class', 'icon')
-        newTask.appendChild(icon);
-
+        const checkboxIcon = document.createElement('span');
+        checkboxIcon.setAttribute('class', 'checkbox-icon')
+        newTask.appendChild(checkboxIcon);
         
-        const newTaskPara = document.createElement('p');
-        newTaskPara.textContent = todo.title;
-        newTask.appendChild(newTaskPara);
+        const newTaskTitle = document.createElement('p');
+        newTaskTitle.textContent = todo.title;
+        newTask.appendChild(newTaskTitle);
+
+        const modifyIcons = document.createElement('div');
+        modifyIcons.setAttribute('class', 'modify-icons');
+
+        newTask.append(modifyIcons);
+
+        const editIcon = document.createElement('span');
+        editIcon.setAttribute('class', 'edit-icon')
+        modifyIcons.appendChild(editIcon);
 
 
+        const trashIcon = document.createElement('span');
+        trashIcon.setAttribute('class', 'trash-icon')
+        modifyIcons.appendChild(trashIcon);
 
 
+    };
 
-        form.newTaskTitle.value = '';
-    });
+    let addNewBranch = function() {
+        const branchList = document.querySelector('.branch-list')
+        const branch = new Branch(branchForm.newBranchTitle.value);
+
+        const newBranchTitle = document.createElement('div');
+        const newBranchPara = document.createElement('p');
+
+        const branchIcon = document.createElement('span');
+        branchIcon.setAttribute('class', 'branch-icon');
+        newBranchTitle.appendChild(branchIcon);
+
+        newBranchPara.textContent = branch.title;
+        newBranchTitle.appendChild(newBranchPara);
+        branchList.appendChild(newBranchTitle);
+
+
+    };
+
+    // Clear form
+    let clearTaskForm = function() {
+        taskForm.newTaskTitle.value = '';
+    };
+
+    let clearBranchForm = function() {
+        branchForm.newBranchTitle.value = '';
+    };
 
 
 })();
--- src/modules/todoGenerator.js
+++ src/modules/todoGenerator.js
@@ -1,7 +1,8 @@
 class Todo {
 
-    constructor(title, dueDate, priority, status) {
+    constructor(title, branch, dueDate, priority, status) {
         this.title = title;
+        this.branch = branch;
         this.dueDate = dueDate;
         this.priority = priority;
         this.status = status;
